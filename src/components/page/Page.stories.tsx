bfd1ca72faa374ef43f5082a8be6e0487be0156f
Nick Pang <nick95927@gmail.com>
2024-09-04 09:40:21 +1000

    Updated page stories to fix build errors

--- src/components/page/Page.stories.tsx
+++ src/components/page/Page.stories.tsx
@@ -52,24 +52,28 @@ const data = {
   ],
   past_incidents: [
     {
-      date: "2024-04-16T10:53:42+1000",
-      service: "PB Server",
+      name: "PB Server",
       reason: "service disrupted",
+      reported: "2024-04-16T10:53:42+1000",
+      restored: "2024-04-18T00:53:42+1000",
     },
     {
-      date: "2024-02-12T09:43:42+1000",
-      service: "HPC-FS",
+      name: "HPC-FS",
       reason: "Scheduled maintenance",
+      reported: "2024-02-12T09:43:42+1000",
+      restored: "2024-02-13T12:43:42+1000",
     },
     {
-      date: "2023-12-13T06:33:42+1000",
-      service: "PB Server",
+      name: "PB Server",
       reason: "service disrupted",
+      reported: "2023-12-13T06:33:42+1000",
+      restored: "2023-12-14T00:33:42+1000",
     },
     {
-      date: "2023-10-19T07:33:32+1000",
-      service: "HPC-FS",
+      name: "HPC-FS",
       reason: "Scheduled maintenance",
+      reported: "2023-10-19T07:33:32+1000",
+      restored: "2023-10-21T00:13:32+1000",
     },
   ],
 };
