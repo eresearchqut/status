c23de2076991d0419fa39e496d2c4e2f00ccc5d7
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-01-06 12:50:57 +1000

    refactor: Use Umami api to create website

--- .flyway/V1_0_1__umami-website.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO website (website_id, name, domain, user_id, created_by, created_at, updated_at)
-SELECT '8c4453a6-4c48-4889-9e89-f3b4f13753bb', 'Status', 'localhost:3000', users.user_id, users.user_id, current_timestamp, current_timestamp
-FROM "user" as users
-WHERE users.username = 'admin'
-  ON CONFLICT (website_id)
-DO NOTHING
